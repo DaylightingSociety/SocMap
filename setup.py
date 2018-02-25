f8e909297eabfc4d6af4bfcbadaaee4952d0eb3a
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2018-02-25 00:01:45 -0500

    Includes more package information

--- setup.py
+++ setup.py
@@ -3,12 +3,25 @@ import os
 from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
+NAME = 'socmap'
+DESCRIPTION = 'Build maps of Twitter communities'
+URL = 'https://github.com/DaylightingSociety/socmap'
+EMAIL = 'socmap@daylightingsociety.org'
+AUTHOR = 'Daylighting Society'
+VERSION = '0.1.0'
+
 install_requires = [
 	"tweepy",
 	"networkx",
 	"jsonpickle",
 	]
 
-setup(name="SocMap",
+setup(
+	name=NAME,
+	version=VERSION,
+	description=DESCRIPTION,
+	author=AUTHOR,
+	author_email=EMAIL,
+	url=URL,
 	install_requires=install_requires
-	)
+)
