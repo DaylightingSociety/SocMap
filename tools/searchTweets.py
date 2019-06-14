1919c3c280765eab4a7458e26b7db5e4c2c98736
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2019-06-14 16:14:25 -0400

    Added optional username list to searchTweets

--- tools/searchTweets.py
+++ tools/searchTweets.py
@@ -7,17 +7,26 @@ sys.path.append(folder + "/..") # Allow us to import files from one level up
 from acquire import Tweet, loadTweetsFromFile
 
 if __name__ == "__main__":
-	if( len(sys.argv) != 3 ):
-		print("USAGE: %s <search_term> <tweetdir>" % sys.argv[0])
+	if( len(sys.argv) < 3 ):
+		print("USAGE: %s <search term as regular expression> <tweetdir> [username] ..." % sys.argv[0])
 		sys.exit(1)
 	term = sys.argv[1]
-	tweetdir = sys.argv[2]
 	search = re.compile(term)
+	tweetdir = sys.argv[2]
 
-	for filename in os.listdir(tweetdir):
-		uname = os.path.basename(filename).split(".")[0]
-		tweets = loadTweetsFromFile(uname, tweetdir)
-		#print("User '%s' tweets: %d" % (uname, len(tweets)))
-		for tweet in tweets:
-			if search.match(tweet.text):
-				print(tweet.text)
+	# If provided a list of users, check those
+	if( len(sys.argv) >= 4 ):
+		for username in sys.argv[3:]:
+			tweets = loadTweetsFromFile(username, tweetdir)
+			for tweet in tweets:
+				if search.match(tweet.text):
+					print(tweet.text)
+
+	# Otherwise, check all users in the tweetdir by default
+	else:
+		for filename in os.listdir(tweetdir):
+			username = os.path.basename(filename).split(".")[0]
+			tweets = loadTweetsFromFile(username, tweetdir)
+			for tweet in tweets:
+				if search.match(tweet.text):
+					print(tweet.text)
